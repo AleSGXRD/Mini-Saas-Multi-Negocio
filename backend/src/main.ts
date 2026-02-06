0555eaeebdd80490f51c38eb6c5f5c2966a180d6
AleSGXRD <alesgxrd@gmail.com>
2026-02-05 23:28:31 -0300

    fix rawbody

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +8,15 @@ async function bootstrap() {
     origin: 'http://localhost:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
+
+  app.use(
+    bodyParser.json({
+      verify: (req: any, _res, buf) => {
+        req.rawBody = buf;
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   CLERK_PUBLISHABLE_KEY: 'pk_test_YWNjZXB0ZWQtc3BpZGVyLTAuY2xlcmsuYWNjb3VudHMuZGV2JA',
-  SERVER_URL: 'https://uncomplemented-latia-forkedly.ngrok-free.dev'
+  SERVER_URL: 'https://mini-saas-multi-negocio.onrender.com'
 }
